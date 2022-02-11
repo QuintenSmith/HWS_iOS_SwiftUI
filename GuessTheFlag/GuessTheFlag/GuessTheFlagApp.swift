b720c4d96fdcdb18258db25cede2f5266179a6ee
QuintenSmith <quintensmith15@gmail.com>
2022-02-10 20:33:23 -0800

    Added GuessTheFlag app

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Quinten Smith on 2/10/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
