c74edde94ccf0feefb75b23755c8b3d74829551f
QuintenSmith <quintensmith15@gmail.com>
2022-02-12 14:23:09 -0800

    Complete ViewAndModifiers Project

---
+++ ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Quinten Smith on 2/10/22.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
