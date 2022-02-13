f0e7041736a51a5437425b049601480b457863e6
QuintenSmith <quintensmith15@gmail.com>
2022-02-12 19:21:34 -0800

    Build BetterRest with CoreML

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Quinten Smith on 2/12/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
